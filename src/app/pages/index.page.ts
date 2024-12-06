e742ee183d765dffe0ef6c84488093ba4a46b61f
Brandon Roberts <robertsbt@gmail.com>
2024-12-06 10:52:58 -0600

    feat: update app index page

--- src/app/pages/index.page.ts
+++ src/app/pages/index.page.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   template: `
+    <div>
+      <a href="https://analogjs.org/" target="_blank">
+        <img alt="Analog Logo" class="logo analog" src="/analog.svg" />
+      </a>
+    </div>
+
     <h2>Analog</h2>
 
     <h3>The fullstack meta-framework for Angular!</h3>
+
+    <div class="card">
+      <button type="button" (click)="increment()">Count {{ count() }}</button>
+    </div>
 
     <p class="read-the-docs">
       <a href="https://analogjs.org" target="_blank">Docs</a> |
@@ -17,11 +27,12 @@ import { Component } from '@angular/core';
     </p>
   `,
   styles: `
-    :host {
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
+    .logo {
+      will-change: filter;
+    }
+
+    .logo:hover {
+      filter: drop-shadow(0 0 2em #646cffaa);
     }
 
     .read-the-docs > * {
@@ -35,4 +46,10 @@ import { Component } from '@angular/core';
     }
   `,
 })
-export default class HomeComponent {}
+export default class HomeComponent {
+  count = signal(0);
+
+  increment() {
+    this.count.update((count) => count + 1);
+  }
+}
